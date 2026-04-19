09a478292c8d87a8a13be91a26ff7ec6bba8a5b6
natanimn <natanimco@gmail.com>
2026-04-20 00:22:23 +0300

    feat: add CreateAccountRequestDTO dto class

---
+++ core/src/main/java/et/edu/astu/core/dtos/CreateAccountRequestDTO.java
@@ -0,0 +1,20 @@
+package et.edu.astu.core.dtos;
+
+import java.time.LocalDateTime;
+
+/**
+ * Account creation DTO class. Account number is generated automatically.
+ * @param firstName first name
+ * @param middleName father name
+ * @param lastName grandfather name
+ * @param initialBalance initial deposit money
+ * @param birthDate birthdate
+ * @author Natanim
+ */
+public record CreateAccountRequestDTO(
+        String firstName,
+        String middleName,
+        String lastName,
+        int initialBalance,
+        LocalDateTime birthDate
+) { }
