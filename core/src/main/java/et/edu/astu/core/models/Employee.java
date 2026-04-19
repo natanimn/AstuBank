614360b780cde3cc159f86910ab1c1389cd7140c
natanimn <natanimco@gmail.com>
2026-04-20 00:23:49 +0300

    feat: add Employee model class

---
+++ core/src/main/java/et/edu/astu/core/models/Employee.java
@@ -0,0 +1,66 @@
+package et.edu.astu.core.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * An employee model class.
+ * @author Natanim
+ */
+@Entity
+@Table(
+        name = "employees",
+        indexes = {
+                @Index(
+                        name = "username_employee_indx",
+                        columnList = "username"
+                )
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @JsonIgnore
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "middle_name", nullable = false)
+    private String middleName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "created_at", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private LocalDateTime createdAt;
+
+    public Employee(String username, String password, String firstName, String middleName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    @PrePersist
+    private void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
+}
