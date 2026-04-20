f81845cbfe51901a3ee4f9625be4d14038f15f78
natanimn <natanimco@gmail.com>
2026-04-21 00:26:08 +0300

    feat: add methods findByAccountNumber, findByPhoneNumber

--- core/src/main/java/et/edu/astu/core/repositories/AccountRepository.java
+++ core/src/main/java/et/edu/astu/core/repositories/AccountRepository.java
@@ -2,7 +2,16 @@ package et.edu.astu.core.repositories;
 
 import et.edu.astu.core.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Long> { }
+public interface AccountRepository extends JpaRepository<Account, String> {
+    Optional<Account> findByAccountNumber(Long accountNumber);
+
+    @Query("SELECT a FROM Account a WHERE a.phone = :phone AND a.phone_search = TRUE")
+    Optional<Account> findByPhoneNumber(@Param("phone") String phone);
+}
