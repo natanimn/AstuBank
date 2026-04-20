89c5c08f4e508cc88f18b31f80d5e5f9d8790968
natanimn <natanimco@gmail.com>
2026-04-21 00:33:34 +0300

    feat: add method encoder. Add  annotation @EnableMethodSecurity, @EnableWebSecurity

--- core/src/main/java/et/edu/astu/core/configs/SecurityConfig.java
+++ core/src/main/java/et/edu/astu/core/configs/SecurityConfig.java
@@ -2,17 +2,22 @@ package et.edu.astu.core.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security){
         return security.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(http -> http
-                        .requestMatchers("/api/login/**", "/api/admin/**")
+                        .requestMatchers("/api/auth/e/login/", "/api/auth/u/login/", "/api/admin/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
@@ -20,5 +25,10 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
     }
 }
