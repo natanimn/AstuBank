4f587e2f5631f928ca44ba96769a5a3c51e936d5
natanimn <natanimco@gmail.com>
2026-04-21 00:35:37 +0300

    feat: add EmployeeGenerator generator class

---
+++ core/src/main/java/et/edu/astu/core/generators/EmployeeGenerator.java
@@ -0,0 +1,24 @@
+package et.edu.astu.core.generators;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+public class EmployeeGenerator {
+
+    public String generateUsername(long lastCount){
+        int year = LocalDateTime.now().getYear() % 100;
+        return "BE/%04d/%d".formatted(lastCount + 1, year);
+    }
+
+    public String generatePassword(){
+        StringBuilder passwords = new StringBuilder();
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        String passwordKeys = "abcdefghijklmnopqrstuvwxyz@!0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i<6; i++)
+            passwords.append(passwordKeys.indexOf(random.nextInt(0, passwordKeys.length())));
+        return passwords.toString();
+    }
+}
