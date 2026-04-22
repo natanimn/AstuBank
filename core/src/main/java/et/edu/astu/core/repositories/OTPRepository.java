02f96f5b63ffe125cbe34c041265ff0af835d529
natanimn <natanimco@gmail.com>
2026-04-22 23:30:23 +0300

    refactor: change return type to OTP

--- core/src/main/java/et/edu/astu/core/repositories/OTPRepository.java
+++ core/src/main/java/et/edu/astu/core/repositories/OTPRepository.java
@@ -1,7 +1,6 @@
 package et.edu.astu.core.repositories;
 
 import et.edu.astu.core.models.otp.OTP;
-import et.edu.astu.core.models.otp.UserLoginOTP;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface OTPRepository extends JpaRepository<OTP, Long> {
-    @Query(value = "SELECT * FROM temporary_otp WHERE account_number = :ac AND user_id = :id", nativeQuery = true)
-    Optional<UserLoginOTP> findOTP(@Param("ac") long accountNumber, @Param("id") long userId);
+    @Query(value = "SELECT * FROM temporary_otp WHERE account_number = :ac", nativeQuery = true)
+    Optional<OTP> findOTP(@Param("ac") long accountNumber);
 }
