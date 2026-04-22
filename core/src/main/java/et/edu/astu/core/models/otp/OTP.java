1288f31128caf5e8ef89bedcb2685e544f3c6c54
natanimn <natanimco@gmail.com>
2026-04-22 23:29:57 +0300

    feat: add field accountNumber

--- core/src/main/java/et/edu/astu/core/models/otp/OTP.java
+++ core/src/main/java/et/edu/astu/core/models/otp/OTP.java
@@ -2,25 +2,17 @@ package et.edu.astu.core.models.otp;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(
-        name = "temporary_otp",
-        uniqueConstraints = {
-                @UniqueConstraint(
-                        columnNames = {
-                                "account_number",
-                                "user_id"
-                        }
-                )
-        }
-)
+@Table(name = "temporary_otp")
 @Getter
 @Setter
 @DiscriminatorColumn(name = "otp_type")
+@NoArgsConstructor
 public class OTP {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +23,13 @@ public class OTP {
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name = "account_number", nullable = false, unique = true)
+    private Long accountNumber;
+
+    public OTP(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 
     @PrePersist
     private void onCreate(){
