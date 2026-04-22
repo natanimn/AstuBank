8071fd163830ceaf1f509b90a1207c19d8d5f310
natanimn <natanimco@gmail.com>
2026-04-22 23:27:58 +0300

    feat: add EmployeeController controller class

---
+++ core/src/main/java/et/edu/astu/core/controllers/employee/EmployeeController.java
@@ -0,0 +1,26 @@
+package et.edu.astu.core.controllers.employee;
+
+import et.edu.astu.core.dtos.EmployeeLoginRequest;
+import et.edu.astu.core.dtos.LoginResponse;
+import et.edu.astu.core.services.AccountService;
+import et.edu.astu.core.services.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/e")
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final AccountService accountService;
+    private final EmployeeService service;
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody EmployeeLoginRequest request){
+        return new ResponseEntity<>(service.login(request), HttpStatus.OK);
+    }
+}
