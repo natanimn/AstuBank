8c27f5c08c42bfa3e038907c6b3f8a9dba117b49
natanimn <natanimco@gmail.com>
2026-04-22 23:26:08 +0300

    feat: add CorsConfig config class

---
+++ core/src/main/java/et/edu/astu/core/configs/CorsConfig.java
@@ -0,0 +1,18 @@
+package et.edu.astu.core.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
+        return source;
+    }
+}
