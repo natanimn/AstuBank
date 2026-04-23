24088f2e800e3dd2d21b7144a76482df493da144
natanimn <natanimco@gmail.com>
2026-04-23 20:47:35 +0300

    fix: fix imports

--- core/src/main/java/et/edu/astu/core/models/Employee.java
+++ core/src/main/java/et/edu/astu/core/models/Employee.java
@@ -2,7 +2,15 @@ package et.edu.astu.core.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
