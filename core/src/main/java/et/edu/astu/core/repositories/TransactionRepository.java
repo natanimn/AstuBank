5e165542940893da11d9711ad5e226e246dbc241
natanimn <natanimco@gmail.com>
2026-04-23 20:51:21 +0300

    feat: add methods findTransactions, findByTransactionId

--- core/src/main/java/et/edu/astu/core/repositories/TransactionRepository.java
+++ core/src/main/java/et/edu/astu/core/repositories/TransactionRepository.java
@@ -1,8 +1,24 @@
 package et.edu.astu.core.repositories;
 
+import et.edu.astu.core.interfaces.TransactionResponse;
 import et.edu.astu.core.models.transactions.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> { }
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    @Query(
+            value = "SELECT transaction_id as transactionId, amount, transaction_type as type, created_at as createdAt " +
+                    "FROM transactions WHERE account_id = :ac",
+            nativeQuery = true
+    )
+    List<TransactionResponse> findTransactions(@Param("ac") Long ac, Pageable pageable);
+
+    Optional<Transaction> findByTransactionId(String trxId);
+}
