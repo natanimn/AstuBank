cf84e0c3439dfd8b2d559de1ed8c39632309218e
natanimn <natanimco@gmail.com>
2026-04-23 20:46:17 +0300

    feat: add AuthController controller class

---
+++ core/src/main/java/et/edu/astu/core/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package et.edu.astu.core.controllers;
+
+import et.edu.astu.core.dtos.EmployeeLoginRequest;
+import et.edu.astu.core.dtos.LoginResponse;
+import et.edu.astu.core.dtos.UserLoginOTPRequest;
+import et.edu.astu.core.services.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private AuthService service;
+
+    @PostMapping("/user/login")
+    public ResponseEntity<LoginResponse> userLogin(@RequestBody UserLoginOTPRequest request){
+        return new ResponseEntity<>(service.loginUser(request), HttpStatus.OK);
+    }
+
+    @PostMapping("/employee/login")
+    public ResponseEntity<LoginResponse> employeeLogin(@RequestBody EmployeeLoginRequest request){
+        return new ResponseEntity<>(service.loginEmployee(request), HttpStatus.OK);
+    }
+}
