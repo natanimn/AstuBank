2c89a88994ec87000f7c24b04536456e8d637e61
natanimn <natanimco@gmail.com>
2026-04-23 20:48:25 +0300

    feat: add methods deposit, withdraw, transfer, findTransactions, disconnectTelegram

--- core/src/main/java/et/edu/astu/core/controllers/employee/EmployeeAccountController.java
+++ core/src/main/java/et/edu/astu/core/controllers/employee/EmployeeAccountController.java
@@ -2,14 +2,30 @@ package et.edu.astu.core.controllers.employee;
 
 import et.edu.astu.core.dtos.AccountResponse;
 import et.edu.astu.core.dtos.CreateAccountRequest;
+import et.edu.astu.core.dtos.DepositRequest;
+import et.edu.astu.core.dtos.DepositResponse;
 import et.edu.astu.core.dtos.OTPResponse;
+import et.edu.astu.core.dtos.TransferRequest;
+import et.edu.astu.core.dtos.TransferResponse;
 import et.edu.astu.core.interfaces.CustomerResponse;
+import et.edu.astu.core.interfaces.TransactionResponse;
 import et.edu.astu.core.services.AccountService;
 import et.edu.astu.core.services.OTPService;
+import et.edu.astu.core.services.TransactionService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/e/account")
@@ -17,6 +33,7 @@ import org.springframework.web.bind.annotation.*;
 public class EmployeeAccountController {
     private final AccountService accountService;
     private final OTPService otpService;
+    private final TransactionService transactionService;
 
     @PostMapping("/create")
     public ResponseEntity<AccountResponse> create(@RequestBody CreateAccountRequest request){
@@ -31,5 +48,33 @@ public class EmployeeAccountController {
     @PostMapping("/{ac}/generate/otp")
     public ResponseEntity<OTPResponse> generateOTP(@PathVariable Long ac){
         return new ResponseEntity<>(otpService.generateUserLoginOtp(ac), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/deposit")
+    public ResponseEntity<DepositResponse> deposit(HttpServletRequest http, @RequestBody DepositRequest request){
+        String employeeId = http.getAttribute("employeeId").toString();
+        return new ResponseEntity<>(transactionService.deposit(employeeId, request), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/withdraw")
+    public ResponseEntity<DepositResponse> withdraw(HttpServletRequest http, @RequestBody DepositRequest request){
+        String employeeId = http.getAttribute("employeeId").toString();
+        return new ResponseEntity<>(transactionService.withdraw(employeeId, request), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<TransferResponse> transfer(@RequestBody TransferRequest request){
+        return new ResponseEntity<>(transactionService.transfer(request), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{ac}/transactions")
+    public ResponseEntity<List<TransactionResponse>> findTransactions(@PathVariable Long ac, @PageableDefault Pageable pageable){
+        return new ResponseEntity<>(transactionService.findTransaction(ac, pageable), HttpStatus.OK);
+    }
+
+    @PostMapping("/{ac}/disconnect/telegram")
+    public ResponseEntity<AccountResponse> disconnectTelegram(@PathVariable Long ac){
+        accountService.unlinkWithTelegram(ac);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
