f171e791a10730feaa2cca342e146bd569da3730
natanimn <natanimco@gmail.com>
2026-04-24 23:13:57 +0300

    feat: add cors, filterBefore

--- core/src/main/java/et/edu/astu/core/configs/SecurityConfig.java
+++ core/src/main/java/et/edu/astu/core/configs/SecurityConfig.java
@@ -1,18 +1,25 @@
 package et.edu.astu.core.configs;
 
+import et.edu.astu.core.security.JwtAuthorizer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthorizer authorizer;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security){
         return security.csrf(AbstractHttpConfigurer::disable)
@@ -23,13 +30,12 @@ public class SecurityConfig {
                         .hasRole("EMPLOYEE")
                         .requestMatchers("/api/u/**")
                         .hasRole("USER")
-                        .requestMatchers("/api/admin/**")
-                        .hasRole("ADMIN")
                         .anyRequest()
                         .authenticated()
                 )
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .addFilterBefore(authorizer, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
