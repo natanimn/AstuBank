01cf36c2ff3b7fccc93199024b4858795387945e
natanimn <natanimco@gmail.com>
2026-04-25 23:12:39 +0300

    feat: add method findUserAccount

--- core/src/main/java/et/edu/astu/core/services/UserService.java
+++ core/src/main/java/et/edu/astu/core/services/UserService.java
@@ -1,11 +1,14 @@
 package et.edu.astu.core.services;
 
 import et.edu.astu.common.dto.LoginResponse;
+import et.edu.astu.common.dto.UserAccountResponse;
 import et.edu.astu.common.dto.UserLoginOTPRequest;
 import et.edu.astu.common.dto.UserResponse;
+import et.edu.astu.core.models.Account;
 import et.edu.astu.core.models.CustomUserDetails;
 import et.edu.astu.core.models.User;
 import et.edu.astu.core.repositories.UserRepository;
+import et.edu.astu.core.mapper.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -35,6 +38,14 @@ public class UserService {
 
     public User getUser(Long userId){
         return repository.findByUserId(userId).orElse(null);
+    }
+
+    public UserAccountResponse findUserAccount(Long userId){
+        User user = getUser(userId);
+        Account account = user.getAccount();
+        if (account == null)
+            throw new RuntimeException("No account connected");
+        return Mapper.mapUserAccount(account);
     }
 
     public CustomUserDetails getCustomUser(Long userId){
