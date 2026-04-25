a5aa5afb4d4d992dda88782f702d54f5f4225a1e
natanimn <natanimco@gmail.com>
2026-04-25 22:58:51 +0300

    feat: remover method map

---
+++ common/src/main/java/et/edu/astu/common/dto/AccountResponse.java
@@ -0,0 +1,11 @@
+package et.edu.astu.common.dto;
+
+/**
+ * Describes response of an account
+ * @param accountNumber account number
+ * @param firstName first name
+ * @param middleName father name
+ * @param lastName grandfather name
+ * @author Natanim
+ */
+public record AccountResponse(Long accountNumber, String firstName, String middleName, String lastName) { }
