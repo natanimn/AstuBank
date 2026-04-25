38a00dadad2f4a86ae5e63636f7d658db7ac0150
natanimn <natanimco@gmail.com>
2026-04-25 23:11:17 +0300

    feat: add account number automatically

--- core/src/main/java/et/edu/astu/core/controllers/user/TransactionUserAccountController.java
+++ core/src/main/java/et/edu/astu/core/controllers/user/TransactionUserAccountController.java
@@ -3,6 +3,7 @@ package et.edu.astu.core.controllers.user;
 import et.edu.astu.common.dto.TransferRequest;
 import et.edu.astu.common.dto.TransferResponse;
 import et.edu.astu.core.services.TransactionService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,9 @@ public class TransactionUserAccountController {
     private final TransactionService service;
 
     @PostMapping("/transfer")
-    public ResponseEntity<TransferResponse> transfer(@RequestBody TransferRequest request){
-        return new ResponseEntity<>(service.transfer(request), HttpStatus.CREATED);
+    public ResponseEntity<TransferResponse> transfer(HttpServletRequest http, @RequestBody TransferRequest request){
+        Long account = (Long) http.getAttribute("account");
+        TransferRequest transferRequest = new TransferRequest(account, request.receiver(), request.amount());
+        return new ResponseEntity<>(service.transfer(transferRequest), HttpStatus.CREATED);
     }
 }
