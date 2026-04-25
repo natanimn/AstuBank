30e76d8dfd6c0221be845c3a2a44f42caa6e85bc
natanimn <natanimco@gmail.com>
2026-04-25 23:10:03 +0300

    feat: change package core to common

--- core/src/main/java/et/edu/astu/core/repositories/TransactionRepository.java
+++ core/src/main/java/et/edu/astu/core/repositories/TransactionRepository.java
@@ -1,6 +1,6 @@
 package et.edu.astu.core.repositories;
 
-import et.edu.astu.core.interfaces.TransactionResponse;
+import et.edu.astu.common.interfaces.TransactionResponse;
 import et.edu.astu.core.models.transactions.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
