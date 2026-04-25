3b82f9ff704d583e8f097fb2f047b654ccb0e9da
natanimn <natanimco@gmail.com>
2026-04-25 23:08:55 +0300

    feat: add Mapper mapper class

---
+++ bot/src/main/java/et/edu/astu/bot/helpers/Mapper.java
@@ -0,0 +1,20 @@
+package et.edu.astu.bot.helpers;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import et.edu.astu.bot.http.ApiResponse;
+
+import java.lang.reflect.Type;
+
+public abstract class Mapper {
+    private final static Gson gson;
+
+    static {
+        gson = new Gson();
+    }
+
+    public static <T> ApiResponse<T> map(String json, Type type){
+        Type resultType = TypeToken.getParameterized(ApiResponse.class, type).getType();
+        return gson.fromJson(json, resultType);
+    }
+}
