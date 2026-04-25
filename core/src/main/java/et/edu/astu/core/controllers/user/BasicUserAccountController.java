703f527e4921379fe29f85444d0d729744dd57aa
natanimn <natanimco@gmail.com>
2026-04-25 23:00:52 +0300

    feat: add methods getMyAccount, searchAccount

--- core/src/main/java/et/edu/astu/core/controllers/user/BasicUserAccountController.java
+++ core/src/main/java/et/edu/astu/core/controllers/user/BasicUserAccountController.java
@@ -1,7 +1,10 @@
 package et.edu.astu.core.controllers.user;
 
+import et.edu.astu.common.dto.AccountResponse;
+import et.edu.astu.common.dto.UserAccountResponse;
 import et.edu.astu.common.dto.UserResponse;
-import et.edu.astu.core.interfaces.TransactionResponse;
+import et.edu.astu.common.interfaces.TransactionResponse;
+import et.edu.astu.core.services.AccountService;
 import et.edu.astu.core.services.TransactionService;
 import et.edu.astu.core.services.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.print.Pageable;
@@ -24,11 +28,19 @@ import java.util.List;
 public class BasicUserAccountController {
     private final UserService service;
     private final TransactionService transactionService;
+    private final AccountService accountService;
 
     @GetMapping("/me")
     public ResponseEntity<UserResponse> getMe(HttpServletRequest request){
         Long userId = (Long) request.getAttribute("userId");
         return new ResponseEntity<>(service.getMe(userId), HttpStatus.OK);
+    }
+
+    @GetMapping("/my/account")
+    @PreAuthorize("hasAuthority('connected')")
+    public ResponseEntity<UserAccountResponse> getMyAccount(HttpServletRequest request){
+        Long userId = (Long) request.getAttribute("userId");
+        return new ResponseEntity<>(service.findUserAccount(userId), HttpStatus.OK);
     }
 
     @GetMapping("/my/transactions")
@@ -42,5 +54,10 @@ public class BasicUserAccountController {
     @PreAuthorize("hasAuthority('connected')")
     public ResponseEntity<Object> myTransaction(@PathVariable String trxId){
         return new ResponseEntity<>(transactionService.findTransaction(trxId), HttpStatus.OK);
+    }
+
+    @GetMapping("/search/account")
+    public ResponseEntity<AccountResponse> searchAccount(@RequestParam String phone){
+        return new ResponseEntity<>(accountService.searchByPhone(phone), HttpStatus.OK);
     }
 }
