608670a8fec2899e360500dc556b8c60c10c345c
natanimn <natanimco@gmail.com>
2026-04-25 23:00:05 +0300

    feat: remove constructor

--- bot/src/main/java/et/edu/astu/bot/http/ApiResponse.java
+++ bot/src/main/java/et/edu/astu/bot/http/ApiResponse.java
@@ -3,10 +3,6 @@ package et.edu.astu.bot.http;
 public class ApiResponse<T> {
     private T result;
 
-    public ApiResponse(T result){
-        this.result = result;
-    }
-
     public T getResult() {
         return result;
     }
