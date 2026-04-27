cf51d67859ad4cacf2403658f4a2b9cf0c045805
natanimn <natanimco@gmail.com>
2026-04-27 13:59:12 +0300

    feat: add Javadocs, add field createdAt

--- common/src/main/java/et/edu/astu/common/dto/TransferResponse.java
+++ common/src/main/java/et/edu/astu/common/dto/TransferResponse.java
@@ -1,10 +1,25 @@
 package et.edu.astu.common.dto;
 
+import java.time.LocalDateTime;
+
+/**
+ * Represents response of Transfer transaction.
+ * @param senderFullName Sender's full name
+ * @param senderAccountNumber Sender's account number
+ * @param receiverFullName Receiver's full name
+ * @param receiverAccountNumber Receiver's account number
+ * @param transactionId Transaction ID
+ * @param amount amount
+ * @param createdAt timestamp
+ *
+ * @author Natanim
+ */
 public record TransferResponse(
         String senderFullName,
         Long senderAccountNumber,
         String receiverFullName,
         Long receiverAccountNumber,
         String transactionId,
-        Double amount
+        Double amount,
+        LocalDateTime createdAt
 ) { }
