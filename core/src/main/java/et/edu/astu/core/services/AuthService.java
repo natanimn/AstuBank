ee4c5df764ae806c4c85179ebf7e7b0ab8869169
natanimn <natanimco@gmail.com>
2026-04-27 13:50:54 +0300

    feat: add loginAdmin method, add fields ADMIN_USERNAME, ADMIN_PASSWORD

--- core/src/main/java/et/edu/astu/core/services/AuthService.java
+++ core/src/main/java/et/edu/astu/core/services/AuthService.java
@@ -1,22 +1,45 @@
 package et.edu.astu.core.services;
 
-import et.edu.astu.common.dto.EmployeeLoginRequest;
+import et.edu.astu.common.dto.AdminLoginResponse;
+import et.edu.astu.common.dto.LoginRequest;
 import et.edu.astu.common.dto.LoginResponse;
 import et.edu.astu.common.dto.UserLoginOTPRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+/**
+ * AuthService class.
+ *
+ * @author Natanim
+ */
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+    @Value("{app.admin.username}")
+    private String ADMIN_USERNAME;
+
+    @Value("{app.admin.password}")
+    private String ADMIN_PASSWORD;
+
     private final EmployeeService employeeService;
     private final UserService userService;
 
-    public LoginResponse loginEmployee(EmployeeLoginRequest request){
+    public LoginResponse loginEmployee(LoginRequest request){
         return employeeService.login(request);
     }
 
     public LoginResponse loginUser(UserLoginOTPRequest request){
         return userService.login(request);
+    }
+
+    public AdminLoginResponse loginAdmin(LoginRequest request){
+        if (request.username() == null || request.password() == null)
+            throw new RuntimeException("Both username and password is required");
+
+        if (request.username().equals(ADMIN_USERNAME) && request.password().equals(ADMIN_PASSWORD))
+            return new AdminLoginResponse(true);
+
+        return new AdminLoginResponse(false);
     }
 }
