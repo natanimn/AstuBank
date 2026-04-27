e4fd952f1df66b96b5491c0fc7c19ce021083071
natanimn <natanimco@gmail.com>
2026-04-27 13:51:40 +0300

    feat: add Cancel handler class

---
+++ bot/src/main/java/et/edu/astu/bot/handlers/Cancel.java
@@ -0,0 +1,36 @@
+package et.edu.astu.bot.handlers;
+
+import et.edu.astu.bot.helpers.KeyboardHelper;
+import et.edu.astu.bot.http.HttpService;
+import et.edu.astu.common.dto.UserResponse;
+import io.github.natanimn.telebof.BotContext;
+import io.github.natanimn.telebof.annotations.MessageHandler;
+import io.github.natanimn.telebof.enums.ChatType;
+import io.github.natanimn.telebof.enums.ParseMode;
+import io.github.natanimn.telebof.types.updates.Message;
+
+/**
+ * Cancel class.
+ * Handles "Cancel" button
+ *
+ * @author Natanim
+ */
+public class Cancel {
+    private final HttpService service = new HttpService();
+
+    @MessageHandler(state = "*", texts = "Cancel", chatType = ChatType.PRIVATE)
+    @MessageHandler(state = "*", commands = "cancel", chatType = ChatType.PRIVATE)
+    private void on(BotContext context, Message message){
+        long userId = message.getFrom().getId();
+        UserResponse response = service.getMe(userId);
+
+        context.sendMessage(
+                userId,
+                "<b>\uD83D\uDEAB Action cancelled</b>"
+        )
+                .parseMode(ParseMode.HTML)
+                .replyMarkup(KeyboardHelper.mainKeyboard(response))
+                .exec();
+        context.clearState(userId);
+    }
+}
