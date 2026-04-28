d0669ea37aafccbda3c878ec4648ace6c6e49709
natanimn <natanimco@gmail.com>
2026-04-29 01:48:40 +0300

    feat: remove findAccount method

--- core/src/main/java/et/edu/astu/core/repositories/AccountRepository.java
+++ core/src/main/java/et/edu/astu/core/repositories/AccountRepository.java
@@ -1,10 +1,7 @@
 package et.edu.astu.core.repositories;
 
-import et.edu.astu.common.interfaces.CustomerResponse;
 import et.edu.astu.core.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,11 +11,4 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByAccountNumber(Long accountNumber);
 
     Optional<Account> findByPhone(String phone);
-
-    @Query(value =
-            "SELECT account_number as accountNumber, first_name as firstName, middle_name as middleName, last_name as lastName, " +
-            "linked_telegram_user_id as telegramUserId, telegram_link_verified as telegramLinkVerified FROM accounts WHERE account_number = :ac",
-     nativeQuery = true
-    )
-    CustomerResponse findAccount(@Param("ac") Long ac);
 }
