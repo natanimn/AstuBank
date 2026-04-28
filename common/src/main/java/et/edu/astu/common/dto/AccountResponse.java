8577e60475af1de7f9a67a4cd8565404d97f5186
natanimn <natanimco@gmail.com>
2026-04-29 01:49:30 +0300

    feat: add field connectedWithTelegram

--- common/src/main/java/et/edu/astu/common/dto/AccountResponse.java
+++ common/src/main/java/et/edu/astu/common/dto/AccountResponse.java
@@ -6,6 +6,13 @@ package et.edu.astu.common.dto;
  * @param firstName first name
  * @param middleName father name
  * @param lastName grandfather name
+ * @param connectedWithTelegram account connection with telegram
  * @author Natanim
  */
-public record AccountResponse(Long accountNumber, String firstName, String middleName, String lastName) { }
+public record AccountResponse(
+        Long accountNumber,
+        String firstName,
+        String middleName,
+        String lastName,
+        boolean connectedWithTelegram
+) { }
