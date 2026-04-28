1f4ecedbbecea8fd00403be5cab0e0160f361d11
natanimn <natanimco@gmail.com>
2026-04-29 01:52:53 +0300

    feat: add log

--- core/src/main/java/et/edu/astu/core/controllers/ExceptionController.java
+++ core/src/main/java/et/edu/astu/core/controllers/ExceptionController.java
@@ -1,6 +1,7 @@
 package et.edu.astu.core.controllers;
 
 import et.edu.astu.common.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
+@Slf4j
 public class ExceptionController {
 
     @ExceptionHandler(NoSuchElementException.class)
@@ -18,11 +20,13 @@ public class ExceptionController {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRuntime(RuntimeException ex){
+        log.error(ex.getMessage());
         return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<ErrorResponse> handleEx(Throwable ex){
+        log.error(ex.getMessage());
         return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
