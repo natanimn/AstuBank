132e0906bc0710578dfed81af7d7d9a1f0bffb69
natanimn <natanimco@gmail.com>
2026-04-30 18:02:17 +0300

    refactor: remove import

--- common/src/main/java/et/edu/astu/common/dto/TransactionResponses.java
+++ common/src/main/java/et/edu/astu/common/dto/TransactionResponses.java
@@ -1,7 +1,5 @@
 package et.edu.astu.common.dto;
 
-import et.edu.astu.common.interfaces.TransactionResponse;
-
 import java.util.List;
 
 public record TransactionResponses(long count, List<TransactionResponse> transactions) { }
