a5b2c842a846bee2a80fcdc730ce72168870068c
natanimn <natanimco@gmail.com>
2026-04-30 23:23:52 +0300

    feat: add annotation field countQuery, change return type

--- core/src/main/java/et/edu/astu/core/repositories/TransactionRepository.java
+++ core/src/main/java/et/edu/astu/core/repositories/TransactionRepository.java
@@ -2,13 +2,13 @@ package et.edu.astu.core.repositories;
 
 import et.edu.astu.common.dto.TransactionResponse;
 import et.edu.astu.core.models.transactions.Transaction;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -20,10 +20,11 @@ import java.util.Optional;
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     @Query(
             value = "SELECT transaction_id as transactionId, amount, transaction_type as type, created_at as createdAt " +
-                    "FROM transactions WHERE account_id = :ac",
+                    "FROM transactions WHERE account_id = :ac ORDER BY created_at DESC",
+            countQuery = "SELECT count(id) FROM transactions WHERE account_id = :ac",
             nativeQuery = true
     )
-    List<TransactionResponse> findTransactions(@Param("ac") Long ac, Pageable pageable);
+    Page<TransactionResponse> findTransactions(@Param("ac") Long ac, Pageable pageable);
 
     Optional<Transaction> findByTransactionId(String trxId);
 
