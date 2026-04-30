1fea42a85cd6f86030dd064ffd622521440e13b9
natanimn <natanimco@gmail.com>
2026-05-01 00:37:06 +0300

    feat: add start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 log()    { echo -e "${BLUE}[INFO]${NC}  $1"; }
 success(){ echo -e "${GREEN}[OK]${NC}    $1"; }
